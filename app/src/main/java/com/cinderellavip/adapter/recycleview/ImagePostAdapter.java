44a82a0bbc85119e9c56e696b5078b41babc9e27
xumingming <835683840@qq.com>
2020-04-29 13:49:54 +0800

    发现帖子和话题页面UI调整

--- app/src/main/java/com/cinderellavip/adapter/recycleview/ImagePostAdapter.java
+++ app/src/main/java/com/cinderellavip/adapter/recycleview/ImagePostAdapter.java
@@ -6,6 +6,9 @@ import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.module.LoadMoreModule;
 import com.chad.library.adapter.base.viewholder.BaseViewHolder;
 import com.cinderellavip.R;
+import com.cinderellavip.ui.BigImageActivity1;
+
+import java.util.ArrayList;
 
 
 public class ImagePostAdapter extends BaseQuickAdapter<String, BaseViewHolder> implements LoadMoreModule {
@@ -19,6 +22,14 @@ public class ImagePostAdapter extends BaseQuickAdapter<String, BaseViewHolder> i
     protected void convert(BaseViewHolder helper,String item) {
         int position = helper.getAdapterPosition();
         ImageView photoView = helper.getView(R.id.photoView);
+        helper.getView(R.id.photoView).setOnClickListener(v -> {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(R.mipmap.demo_comment);
+            list.add(R.mipmap.demo_comment);
+            list.add(R.mipmap.demo_comment);
+            list.add(R.mipmap.demo_comment);
+            BigImageActivity1.launch(getContext(),list,position);
+        });
 
     }
 
