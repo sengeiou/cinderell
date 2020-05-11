496f64e9f33ca0873814156127c7846d44e5042f
xumingming <835683840@qq.com>
2020-05-11 17:13:11 +0800

    数据同步

--- app/src/main/java/com/cinderellavip/http/HttpUrl.java
+++ app/src/main/java/com/cinderellavip/http/HttpUrl.java
@@ -25,7 +25,13 @@ public interface HttpUrl {
     String goods_detail = "api/store/product/info/";//商品详情
     String goods_comment = "api/store/product/comments";//商品评价
     String goods_coupons = "api/store/product/coupons";//商品优惠券
-    String coupons_receive = "api/store/coupon/receive";//商品优惠券
+    String coupons_receive = "api/store/coupon/receive";//领取优惠券
+    //2020-05-11
+    String brand_detail = "api/store/brand/info";//品牌详情
+    String shop_detail = "api/store/brand/info";//店铺详情
+    String goods_for_brand_and_shop = "api/store/product/list/by-store-brand";//品牌 店铺 商品
+    String collect = "api/store/collect/third";//收藏
+    String banner_detail = "api/store/banner/info";//轮播详情
 
 
 }
